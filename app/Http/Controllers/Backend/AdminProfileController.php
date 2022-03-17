9a88d5237c6b20cd8e3e4f28b9254137aefa49f3
Tusher <62649759+tusher719@users.noreply.github.com>
2022-03-17 12:12:30 +0600

    Adding Toster in for view message

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -35,7 +35,13 @@ class AdminProfileController extends Controller
             $data['profile_photo_path'] = $filename;
         }
         $data->save();
-        return redirect()->route('admin.profile');
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
 
 
 
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -17,46 +17,69 @@
     <link rel="stylesheet" href="{{ asset('backend') }}/css/style.css">
     <link rel="stylesheet" href="{{ asset('backend') }}/css/skin_color.css">
 
+    <!-- toastr Cdn -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 </head>
-
 <body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
 
-<div class="wrapper">
-
-    {{-- header part --}}
+    <div class="wrapper">
+        {{-- header part --}}
         @include('admin.body.header')
 
-    <!-- Left side column. contains the logo and sidebar -->
-    @include('admin.body.sidebar')
+        <!-- Left side column. contains the logo and sidebar -->
+        @include('admin.body.sidebar')
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+        <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
 
-        @yield('admin')
+                @yield('admin')
 
+            </div>
+            <!-- /.content-wrapper -->
+        @include('admin.body.footer')
+
+        <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+            <div class="control-sidebar-bg"></div>
 
     </div>
-    <!-- /.content-wrapper -->
-    @include('admin.body.footer')
-
-    <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
-
-</div>
-<!-- ./wrapper -->
+    <!-- ./wrapper -->
 
 
-<!-- Vendor JS -->
-<script src="{{ asset('backend') }}/js/vendors.min.js"></script>
-<script src="{{ asset('assets') }}/icons/feather-icons/feather.min.js"></script>
-<script src="{{ asset('assets') }}/vendor_components/easypiechart/dist/jquery.easypiechart.js"></script>
-<script src="{{ asset('assets') }}/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
-<script src="{{ asset('assets') }}/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
+    <!-- Vendor JS -->
+    <script src="{{ asset('backend') }}/js/vendors.min.js"></script>
+    <script src="{{ asset('assets') }}/icons/feather-icons/feather.min.js"></script>
+    <script src="{{ asset('assets') }}/vendor_components/easypiechart/dist/jquery.easypiechart.js"></script>
+    <script src="{{ asset('assets') }}/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
+    <script src="{{ asset('assets') }}/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
 
-<!-- Sunny Admin App -->
-<script src="{{ asset('backend') }}/js/template.js"></script>
-<script src="{{ asset('backend') }}/js/pages/dashboard.js"></script>
+    <!-- Sunny Admin App -->
+    <script src="{{ asset('backend') }}/js/template.js"></script>
+    <script src="{{ asset('backend') }}/js/pages/dashboard.js"></script>
+    <!-- toastr Script -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
+    <script>
+            @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type','info') }}"
+        switch(type){
+            case 'info':
+                toastr.info(" {{ Session::get('message') }} ");
+                break;
+
+            case 'success':
+                toastr.success(" {{ Session::get('message') }} ");
+                break;
+
+            case 'warning':
+                toastr.warning(" {{ Session::get('message') }} ");
+                break;
+
+            case 'error':
+                toastr.error(" {{ Session::get('message') }} ");
+                break;
+        }
+        @endif
+    </script>
 
 </body>
 </html>
