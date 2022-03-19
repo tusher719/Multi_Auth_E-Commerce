203644e6f71ccce364152379c2067f4b46e2e575
Tusher <62649759+tusher719@users.noreply.github.com>
2022-03-19 10:33:43 +0600

    User profile page setup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Hi... <strong style="color: blue">{{ Auth::user()->name }}</strong>
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('content')
+    <div class="body-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <br>
+                    <img class="card-img-top" src="{{ (!empty($editData->profile_photo_path)) ? url('uploads/admin_images/'.$editData->profile_photo_path) : url('uploads/no_image.jpg') }}" style="border-radius: 50%; height: 100%; width: 100%;">
+                    <br><br>
+                    <ul class="list-group list-group-flush">
+                        <a href="" class="btn btn-primary btn-sm btn-block">Home</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Profile Update</a>
+                        <a href="" class="btn btn-primary btn-sm btn-block">Change Password</a>
+                        <a href="" class="btn btn-danger btn-sm btn-block">Logout</a>
+                    </ul>
+                </div> <!-- End col md 2 -->
 
-    <div class="py-12">
-        <h2>This is home page..!</h2>
+                <div class="col-md-2">
+                    script write
+                </div> <!-- End col md 2 -->
+
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="text-center"><span class="text-danger">Hello World!</span><strong> {{ Auth::user()->name }} </strong> Welcome to Easy Online Shop</div>
+                    </div>
+                </div> <!-- End col md 6 -->
+            </div> <!-- End row -->
+        </div>
     </div>
-</x-app-layout>
+@endsection
