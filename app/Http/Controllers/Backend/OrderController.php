ca2da0febf2f7a40726a917c0d9bb295926b817f
Tusher <62649759+tusher719@users.noreply.github.com>
2022-07-16 16:49:26 +0600

    Read Orders By Status

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -11,19 +11,72 @@ class OrderController extends Controller
 {
     // Pending Orders
     public function PendingOrders(){
-        $total_orders = Order::count();
+        $total_pending = Order::where('status','Pending')->count();
         $orders = Order::where('status','Pending')->orderBy('id','DESC')->get();
-        return view('backend.orders.pending_orders',compact('orders', 'total_orders'));
+        return view('backend.orders.pending_orders',compact('orders', 'total_pending'));
 
     } // end mehtod
 
 
     // Pending Order Details
     public function PendingOrdersDetails($order_id){
-
         $order = Order::with('division','district','state','user')->where('id',$order_id)->first();
         $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
         return view('backend.orders.pending_orders_details',compact('order','orderItem'));
 
     } // end method
+
+    // Confirmed Orders
+    public function ConfirmedOrders(){
+        $total_orders_confirm = Order::where('status','Confirm')->count();
+        $orders = Order::where('status','confirmed')->orderBy('id','DESC')->get();
+        return view('backend.orders.confirmed_orders',compact('orders','total_orders_confirm'));
+
+    } // end mehtod
+
+
+    // Processing Orders
+    public function ProcessingOrders(){
+        $total_orders_pro = Order::where('status','Processing')->count();
+        $orders = Order::where('status','processing')->orderBy('id','DESC')->get();
+        return view('backend.orders.processing_orders',compact('orders','total_orders_pro'));
+
+    } // end mehtod
+
+
+    // Picked Orders
+    public function PickedOrders(){
+        $total_orders_picked = Order::where('status','Picked')->count();
+        $orders = Order::where('status','picked')->orderBy('id','DESC')->get();
+        return view('backend.orders.picked_orders',compact('orders','total_orders_picked'));
+
+    } // end mehtod
+
+
+
+    // Shipped Orders
+    public function ShippedOrders(){
+        $total_orders_shipped = Order::where('status','Shipped')->count();
+        $orders = Order::where('status','shipped')->orderBy('id','DESC')->get();
+        return view('backend.orders.shipped_orders',compact('orders','total_orders_shipped'));
+
+    } // end mehtod
+
+
+    // Delivered Orders
+    public function DeliveredOrders(){
+        $total_orders_Delivered = Order::where('status','Delivered')->count();
+        $orders = Order::where('status','delivered')->orderBy('id','DESC')->get();
+        return view('backend.orders.delivered_orders',compact('orders','total_orders_Delivered'));
+
+    } // end mehtod
+
+
+    // Cancel Orders
+    public function CancelOrders(){
+        $total_orders_cancel = Order::where('status','Cancel')->count();
+        $orders = Order::where('status','cancel')->orderBy('id','DESC')->get();
+        return view('backend.orders.cancel_orders',compact('orders','total_orders_cancel'));
+
+    } // end mehtod
 }
---
+++ resources/views/backend/Orders/processing_orders.blade.php
@@ -0,0 +1,84 @@
+@extends('admin.admin_master')
+@section('admin')
+
+
+    <!-- Content Wrapper. Contains page content -->
+
+    <div class="container-full">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="d-flex align-items-center">
+                <div class="mr-auto">
+                    <h3 class="page-title">Processing Orders</h3>
+                    <div class="d-inline-block align-items-center">
+                        <nav>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
+                                <li class="breadcrumb-item" aria-current="page">Orders</li>
+                                <li class="breadcrumb-item active" aria-current="page">Processing Orders</li>
+
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+
+                <div class="col-12">
+
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Processing Orders List <span class="badge badge-pill badge-primary badge-sm">{{ $total_orders_pro }}</span></h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <div class="table-responsive">
+                                <table id="example1" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>Date </th>
+                                        <th>Invoice </th>
+                                        <th>Amount </th>
+                                        <th>Payment </th>
+                                        <th>Status </th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($orders as $item)
+                                        <tr>
+                                            <td> {{ $item->order_date }}  </td>
+                                            <td> {{ $item->invoice_no }}  </td>
+                                            <td> ${{ $item->amount }}  </td>
+
+                                            <td> {{ $item->payment_method }}  </td>
+                                            <td> <span class="badge badge-pill badge-primary">{{ $item->status }} </span>  </td>
+
+                                            <td width="25%">
+                                                <a href="{{ route('pending.order.details',$item->id) }}" class="btn btn-info" title="Edit Data"><i class="fa fa-eye"></i> </a>
+                                                <a href="{{ route('coupon.delete',$item->id) }}" class="btn btn-danger" title="Delete Data" id="delete">
+                                                    <i class="fa fa-trash"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+    </div>
+
+@endsection
