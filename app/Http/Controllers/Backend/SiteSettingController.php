f05827cd465a52409e87e04dfe0c6fcfffb06c51
Tusher <62649759+tusher719@users.noreply.github.com>
2022-07-22 12:06:38 +0600

    Setup Site Setting Part-1

---
+++ app/Http/Controllers/Backend/SiteSettingController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\SiteSetting;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class SiteSettingController extends Controller
+{
+    public function SiteSetting(){
+        $setting = SiteSetting::find(1);
+        return view('backend.setting.setting_update',compact('setting'));
+    } // end method
+
+
+    public function SiteSettingUpdate(Request $request){
+
+        $setting_id = $request->id;
+
+        if ($request->file('logo')) {
+
+            $image = $request->file('logo');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(139,36)->save('uploads/logo/'.$name_gen);
+            $save_url = 'uploads/logo/'.$name_gen;
+
+            SiteSetting::findOrFail($setting_id)->update([
+                'phone_one' => $request->phone_one,
+                'phone_two' => $request->phone_two,
+                'email' => $request->email,
+                'company_name' => $request->company_name,
+                'company_address' => $request->company_address,
+                'facebook' => $request->facebook,
+                'twitter' => $request->twitter,
+                'linkedin' => $request->linkedin,
+                'youtube' => $request->youtube,
+                'logo' => $save_url,
+
+            ]);
+
+            $notification = array(
+                'message' => 'Setting Updated with Image Successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+
+        }else{
+
+            SiteSetting::findOrFail($setting_id)->update([
+                'phone_one' => $request->phone_one,
+                'phone_two' => $request->phone_two,
+                'email' => $request->email,
+                'company_name' => $request->company_name,
+                'company_address' => $request->company_address,
+                'facebook' => $request->facebook,
+                'twitter' => $request->twitter,
+                'linkedin' => $request->linkedin,
+                'youtube' => $request->youtube,
+            ]);
+
+            $notification = array(
+                'message' => 'Setting Updated Successfully',
+                'alert-type' => 'info'
+            );
+
+            return redirect()->back()->with($notification);
+
+        } // end else
+    } // end method
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\CouponController;
 use App\Http\Controllers\Backend\OrderController;
 use App\Http\Controllers\Backend\ReportController;
+use App\Http\Controllers\Backend\SiteSettingController;
 use App\Http\Controllers\Backend\ShippingAreaController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\SubSubCategoryController;
@@ -231,6 +232,16 @@ Route::prefix('blog')->group(function(){
     Route::get('/add/post', [BlogController::class, 'AddBlogPost'])->name('add.post');
     Route::post('/post/store', [BlogController::class, 'BlogPostStore'])->name('post-store');
 });
+
+
+// Admin Site Setting Routes
+Route::prefix('setting')->group(function(){
+    Route::get('/site', [SiteSettingController::class, 'SiteSetting'])->name('site.setting');
+    Route::post('/site/update', [SiteSettingController::class, 'SiteSettingUpdate'])->name('update.sitesetting');
+
+});
+
+
 
 
 //  Frontend Blog Show Routes
